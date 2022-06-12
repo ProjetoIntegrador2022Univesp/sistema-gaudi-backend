eef4808203fa675e85b1e823aec58af96b85660a
Renan Nalin <nalin.renan@gmail.com>
2022-06-12 18:40:50 -0300

    restarting repo

---
+++ src/main/java/com/gaudisystems/sistemagaudi/SistemaGaudiApplication.java
@@ -0,0 +1,13 @@
+package com.gaudisystems.sistemagaudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGaudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGaudiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaudisystems/sistemagaudi/SistemaGaudiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaudisystems.sistemagaudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGaudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
